3f7525aaffed04880cf24f645ba65f53f9f6eba3
IxxI5 <ioannisxydous@gmail.com>
2020-07-04 22:58:55 +0200

    DbSet -> Comment: Created the Table (PM>add-migration InitialModel)

--- Models/ApplicationDbContext.cs
+++ Models/ApplicationDbContext.cs
@@ -5,7 +5,7 @@ namespace DM.Models
 {
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
-        public DbSet<Incident> Incidents { get; set; }  // Collection of Database context entities (entries)
+        public DbSet<Incident> Incidents { get; set; }  // Creates the Incidents Table in Database on executing the PM> add-migration InitialModel
 
         public ApplicationDbContext() : base("DefaultConnection", throwIfV1Schema: false)
         {
